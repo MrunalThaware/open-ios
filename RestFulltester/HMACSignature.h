fc0b28855a06e288f2b6cae5549c4aa96ce332af
raji16 <sunshine.nair15@gmil.com>
2014-08-07 17:44:28 +0530

    local hmac signature generation file added

---
+++ RestFulltester/HMACSignature.h
@@ -0,0 +1,14 @@
+//
+//  HMACSignature.h
+//  RestFulltester
+//
+//  Created by Raji Nair on 27/06/14.
+//  Copyright (c) 2014 Citrus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonHMAC.h>
+@interface HMACSignature : NSObject
+- (NSString*)generateHMAC:(NSString*)key withData:(NSString*)data;
+
+@end
