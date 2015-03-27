b53edb8d116e62818f44b55dd9e30047425bdb26
mukesh122 <mukesh.patil@citruspay.com>
2015-03-27 20:15:25 +0530

    Added STAGING_MODE flag

--- RestFulltester/CitrusDevKit/MerchantConstants.h
+++ RestFulltester/CitrusDevKit/MerchantConstants.h
@@ -12,17 +12,27 @@
 
 // Sandbox
 #ifdef SANDBOX_MODE
-
+// for sandbox envirement
 #define VanityUrl @"stgcube"
 #define SignInId @"citrus-native-mobile-app-v1"
 #define SignInSecretKey @"83df0e4db17fa7b206f4c36d3f19d6c1"
 #define SubscriptionId @"citrus-native-mobile-subscription"
 #define SubscriptionSecretKey @"3e2288d3a1a3f59ef6f93373884d2ca1"
 #define MerchantAccessKey @"F2VZD1HBS2VVXJPMWO77"
-#define BaseUrl @"http://sandboxadmin.citruspay.com"
+#define BaseUrl @"https://sandboxadmin.citruspay.com"
 
-#else
-//live only for payments
+#elif STAGING_MODE
+// for staging envirement
+#define VanityUrl @"stgcube"
+#define SignInId @"citrus-native-mobile-app-v1"
+#define SignInSecretKey @"83df0e4db17fa7b206f4c36d3f19d6c1"
+#define SubscriptionId @"citrus-native-mobile-subscription"
+#define SubscriptionSecretKey @"3e2288d3a1a3f59ef6f93373884d2ca1"
+#define MerchantAccessKey @"F2VZD1HBS2VVXJPMWO77"
+#define BaseUrl @"https://stgadmin.citruspay.com"
+
+#else 
+// for production envirement
 #define VanityUrl @"rio"
 #define SignInId @"citrus-native-mobile-app-v1"
 #define SignInSecretKey @"83df0e4db17fa7b206f4c36d3f19d6c1"
@@ -30,7 +40,6 @@
 #define SubscriptionSecretKey @"3e2288d3a1a3f59ef6f93373884d2ca1"
 #define MerchantAccessKey @"2GPZFO5FDDLTQY0O98JT"
 #define BaseUrl @"https://admin.citruspay.com"
-
 #endif
 
 
